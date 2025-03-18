d00ee2faa48b705e81857900ad1d0be3d8127c8c
NemchME <mnemch@yandex.ru>
2025-03-19 00:43:32 +0300

    Finish

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -2,11 +2,77 @@ package ru.practicum.dinner;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
 
 public class DinnerConstructor {
-    HashMap<String, ArrayList<String>> dishes;
+    static HashMap<String, ArrayList<String>> dishes;
+    static Scanner sc;
+    static Random rd;
 
-    public DinnerConstructor() {
+    public DinnerConstructor(Scanner scanner) {
+        sc = scanner;
         dishes = new HashMap<>();
+        rd = new Random();
+    }
+
+    public static boolean checkType(String key) {
+        return dishes.containsKey(key);
+    }
+
+    public static void addNewDish() {
+        System.out.println("Введите тип блюда:");
+        String dishType = sc.nextLine();
+        System.out.println("Введите название блюда:");
+        String dishName = sc.nextLine();
+
+        if (checkType(dishType)) {
+            dishes.get(dishType).add(dishName);
+        } else {
+            ArrayList<String> dishNames = new ArrayList<>();
+            dishNames.add(dishName);
+            dishes.put(dishType, dishNames);
+        }
+    }
+
+    public static void generateDishCombo() {
+        System.out.println("Начинаем конструировать обед...");
+
+        System.out.println("Введите количество наборов, которые нужно сгенерировать:");
+        int numberOfCombos = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
+
+        ArrayList<String> necessaryDishes = new ArrayList<>();
+
+        while (true) {
+            String necessaryDish = sc.nextLine();
+
+            if (necessaryDish.isEmpty()) {
+                break;
+            }
+            if (checkType(necessaryDish)) {
+                necessaryDishes.add(necessaryDish);
+            } else {
+                System.out.println("Несуществующий тип блюда!");
+            }
+        }
+
+        int counter = 1;
+        while (numberOfCombos != counter) {
+            System.out.println("Комбо №" + counter);
+            ArrayList<String> combo = new ArrayList<>();
+
+            for (int i = 0; i < necessaryDishes.size(); i++) {
+                String category = necessaryDishes.get(i);
+                ArrayList<String> dishesInCategory = dishes.get(category);
+
+                int index = rd.nextInt(necessaryDishes.size());
+                combo.add(dishesInCategory.get(index));
+            }
+            System.out.println(combo);
+            counter++;
+        }
     }
 }
--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -4,16 +4,15 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+import static ru.practicum.dinner.DinnerConstructor.generateDishCombo;
 
+public class Main {
     static DinnerConstructor dc;
     static Scanner scanner;
-    static Random rd;
 
     public static void main(String[] args) {
-        dc = new DinnerConstructor();
         scanner = new Scanner(System.in);
-        rd = new Random();
+        dc = new DinnerConstructor(scanner);
 
         while (true) {
             printMenu();
@@ -21,10 +20,10 @@ public class Main {
 
             switch (command) {
                 case "1":
-                    addNewDish();
+                    dc.addNewDish();
                     break;
                 case "2":
-                    generateDishCombo();
+                    dc.generateDishCombo();
                     break;
                 case "3":
                     return;
@@ -32,70 +31,10 @@ public class Main {
         }
     }
 
-    public static boolean checkType(String key) {
-        return dc.dishes.containsKey(key);
-    }
-
     private static void printMenu() {
         System.out.println("\nВыберите команду:");
         System.out.println("1 - Добавить новое блюдо");
         System.out.println("2 - Сгенерировать комбинации блюд");
         System.out.println("3 - Выход");
-    }
-
-    private static void addNewDish() {
-        System.out.println("Введите тип блюда:");
-        String dishType = scanner.nextLine();
-        System.out.println("Введите название блюда:");
-        String dishName = scanner.nextLine();
-
-        if (checkType(dishType)) {
-            dc.dishes.get(dishType).add(dishName);
-        } else {
-            ArrayList<String> dishNames = new ArrayList<>();
-            dishNames.add(dishName);
-            dc.dishes.put(dishType, dishNames);
-        }
-    }
-
-    private static void generateDishCombo() {
-        System.out.println("Начинаем конструировать обед...");
-
-        System.out.println("Введите количество наборов, которые нужно сгенерировать:");
-        int numberOfCombos = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
-
-        ArrayList<String> necessaryDishes = new ArrayList<>();
-
-        while (true) {
-            String necessaryDish = scanner.nextLine();
-
-            if (necessaryDish.isEmpty()) {
-                break;
-            }
-            if (checkType(necessaryDish)) {
-                necessaryDishes.add(necessaryDish);
-            } else {
-                System.out.println("Несуществующий тип блюда!");
-            }
-        }
-
-        int counter = 1;
-        while (numberOfCombos != counter) {
-            System.out.println("Комбо №" + counter);
-            ArrayList<String> combo = new ArrayList<>();
-
-            for (int i = 0; i < necessaryDishes.size(); i++) {
-                String category = necessaryDishes.get(i);
-                ArrayList<String> dishesInCategory = dc.dishes.get(category);
-
-                int index = rd.nextInt(necessaryDishes.size());
-                combo.add(dishesInCategory.get(index));
-            }
-            System.out.println(combo);
-            counter++;
-        }
     }
 }
